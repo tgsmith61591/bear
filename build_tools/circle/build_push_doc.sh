d6fc96f90496c98bb06b06f7a073f500e0a48904
tgsmith61591 <tgsmith61591@gmail.com>
2018-11-04 07:40:51 -0600

    CI: Set git user credentials in doc push

--- build_tools/circle/build_push_doc.sh
+++ build_tools/circle/build_push_doc.sh
@@ -32,6 +32,10 @@ else
     git checkout --orphan gh-pages
 fi
 
+# Make sure to set the credentials!
+git config --global user.email "$GH_EMAIL" > /dev/null 2>&1
+git config --global user.name "$GH_NAME" > /dev/null 2>&1
+
 # If the branch gh-pages does not exist, this will have errored
 gh_status=$?
 if [[ ${gh_status} == 1  ]]; then
